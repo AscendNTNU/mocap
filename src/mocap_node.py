b801ee1d4fdff813eee2ceb70b7bd36ccbe2f509
vetleras <vetleras@gmail.com>
2022-04-30 10:29:38 +0200

    Occam's razor go brrr

--- src/mocap_node.py
+++ src/mocap_node.py
@@ -7,29 +7,30 @@ import tf2_ros
 import tf2_geometry_msgs
 from datetime import datetime
 
+rospy.init_node("mocap_node")
+INTERVAL = 0.1
+device = rospy.get_param("~device")
+master = mavutil.mavlink_connection(device)
+buffer = tf2_ros.Buffer()
+listener = tf2_ros.TransformListener(buffer)
 
-def ping(timeout=2):
-    """Pings master and returns wether a reponse was gotten within the timeout"""
-    master.mav.ping_send(0, 0, 0, 0)
-    sent = time.time()
-    while time.time() - sent < timeout:
-        if master.recv_match() is not None:
-            return True
-    return False
-
-
-def set_home_and_origin():
-    master.mav.set_gps_global_origin_send(master.target_system, 599168569, 107284696, 0)
-    master.mav.set_home_position_send(
-        master.target_system, 0, 0, 0, 0, 0, 0, [0, 0, 0, 0], 0, 0, 0
-    )
+receiving = True
+last_received = None
+last_sent = 0.0
 
 
 def callback(msg: PoseStamped):
     global last_received, last_sent
+
+    if last_received is None:
+        rospy.loginfo("First pose received")
     last_received = time.time()
 
-    if time.time() - last_sent < interval:
+    if not receiving:
+        rospy.loginfo("Pose reception resumed")
+        receiving = True
+
+    if time.time() - last_sent < INTERVAL:
         return
 
     transform = buffer.lookup_transform(
@@ -55,38 +56,21 @@ def callback(msg: PoseStamped):
     last_sent = time.time()
 
 
-def check_mavlink_connection(_):
-    if not ping():
-        rospy.logerror(f"Lost connection to {device}")
-        while not ping():
-            pass
-        rospy.logwarn(f"Connection reestablished to {device}")
-        set_home_and_origin()
-    else:
-        rospy.loginfo(f"Connection to {device} is still good")
+def reception_check(_):
+    global receiving
+    if time.time() - last_received > 2 * INTERVAL:
+        receiving = False
+        rospy.logerror("Pose reception stopped")
+        rospy.logwarn("Setting flight mode to land")
+        master.mav.set_mav_cmd_do_set_mode(
+            master.target_system,
+            0,
+            176,
+            0,
+            1,
+        )
 
-
-def check_mocap_reception(_):
-    since_last_pose = time.time() - last_received
-    if since_last_pose > 5 * interval and time.time() - last_logerr > 30:
-        rospy.logerror(f"No pose received in the last {since_last_pose} seconds")
-        last_logerr = time.time()
-
-
-rospy.init_node("mocap_node")
-interval = float(rospy.get_param("~interval"))
-device = rospy.get_param("~device")
-master = mavutil.mavlink_connection(device)
-
-buffer = tf2_ros.Buffer()
-listener = tf2_ros.TransformListener(buffer)
-last_sent = 0.0
-last_received = 0.0
-last_logerr = 0.0
-
-set_home_and_origin()
 
 rospy.Subscriber(rospy.get_param("~topic"), PoseStamped, callback)
-rospy.Timer(rospy.Duration(1), check_mavlink_connection)
-rospy.Timer(rospy.Duration(1), check_mocap_reception)
+rospy.Timer(rospy.Duration(1), reception_check)
 rospy.spin()
