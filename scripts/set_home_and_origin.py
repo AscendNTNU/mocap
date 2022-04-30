af95f44116b215f1958954df775c60bf2bffcfc1
vetleras <vetleras@gmail.com>
2022-04-30 10:30:15 +0200

    Add script

---
+++ scripts/set_home_and_origin.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from pymavlink import mavutil
+
+
+master = mavutil.mavlink_connection("udp:127.0.0.1:14551")
+
+master.mav.set_gps_global_origin_send(master.target_system, 599168569, 107284696, 0)
+master.mav.set_home_position_send(
+    master.target_system, 0, 0, 0, 0, 0, 0, [0, 0, 0, 0], 0, 0, 0
+)
